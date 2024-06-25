bf2567cdd16b4fa970080c4df85d13f955040fb9
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2024-06-25 14:10:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet8 
 ./dotnet8/dotnet --version 
 ./dotnet8/dotnet tool install Excubo.WebCompiler --global
-./dotnet8/dotnet publish src/TryMudBlazor.Client/TryMudBlazor.Client.csproj -c Release -o output
+./dotnet8/dotnet publish src/TryMudBlazor.Client/TryMudBlazor.Client.csproj -c Release -o output --framework net8.0
